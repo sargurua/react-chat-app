eebfe9f2481c7896cc5c8de41aaad9ee47f38bd0
Avinash Sarguru <avinashsarguru@gmail.com>
2020-03-11 12:08:21 -0500

    added socket implementation into backend and frontend

--- backend/app.js
+++ backend/app.js
@@ -4,3 +4,10 @@ var app = express();
 server = app.listen(8080, function(){
     console.log('server is running on port 8080')
 });
+
+var socket = require('socket.io');
+io = socket(server);
+
+io.on('connection', (socket) => {
+    console.log(socket.id);
+});
